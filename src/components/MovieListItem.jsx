ff453c964830a42f453d97bf8306e6047dd88222
Danya <skrepecda998@gmail.com>
2023-08-30 19:13:02 +0300

    done

---
+++ src/components/MovieListItem.jsx
@@ -0,0 +1,24 @@
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+
+import StarRating from "./StarRating";
+import MovieOverview from "./MovieOverview";
+
+function MovieListItem({ movie }) {
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_url}`}
+      />
+      <CardContent>
+        <StarRating rating={movie.vote_average} />
+        <Typography gutterBottom variant="h6" component="h3">
+          {movie.title}
+        </Typography>
+        <MovieOverview>{movie.overview}</MovieOverview>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default MovieListItem;
